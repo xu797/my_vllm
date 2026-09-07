85a185c617edb971adc7714beef8f622d5d04554
xujixian <xjxian02@gmail.com>
2026-09-07 12:50:17 +0800

    tensor init...

---
+++ build/tensor/CMakeFiles/tensor.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tensor.dir/main.cpp.o"
+  "CMakeFiles/tensor.dir/main.cpp.o.d"
+  "CMakeFiles/tensor.dir/src/tensor.cpp.o"
+  "CMakeFiles/tensor.dir/src/tensor.cpp.o.d"
+  "CMakeFiles/tensor.dir/test/test_create_tensor.cpp.o"
+  "CMakeFiles/tensor.dir/test/test_create_tensor.cpp.o.d"
+  "tensor"
+  "tensor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tensor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/tensor/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/xu/work/my_vllm/tensor
+# Build directory: /home/xu/work/my_vllm/build/tensor
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
